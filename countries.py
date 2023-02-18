39c71567d3c55445552dc115e161535f54b94432
Dan <peritract@hotmail.co.uk>
2023-02-18 19:15:00 +0000

    Pythonise & add tests

--- countries.py
+++ countries.py
@@ -1,14 +1,27 @@
-import requests
+from requests import get
 
 
-def print_data(country_data):
-    pass
+class APIError(Exception):
+    """Describes an error triggered by a failing API call."""
 
-def fetch_data(countryName):
+    def __init__(self, message: str, code: int=500):
+        """Creates a new APIError instance."""
+        self.message = message
+        self.code = code
+
+
+def print_data(country_data: dict):
+    """Displays country data from a dict."""
+    print(country_data)
+
+
+def fetch_data(country_name: str) -> dict:
+    """Returns a dict of country data from the API."""
     return {}
 
 
 def main():
+    """Repeatedly prompts the user for country names and displays the result."""
     print(" ")
     print("####################")
     print("Welcome to the REST Countries Searcher")
@@ -23,9 +36,10 @@ def main():
         try:
             country_data = fetch_data(entry)
             print_data(country_data)
-        except Exception as e:
-            print(e.args[0])
+        except APIError as e:
+            print(e.message)
         print(" ")
+
 
 if __name__ == "__main__":
     main()

6dfaa076f16f680a00c0b7e76596dfba80b06e8b
JSackey101 <jeffsackey101@gmail.com>
2025-01-22 16:49:15 +0000

    Updated fetch_data function.

--- countries.py
+++ countries.py
@@ -18,12 +18,18 @@ def print_data(country_data: dict):
 def fetch_data(country_name: str) -> dict:
     """Returns a dict of country data from the API."""
     response = get(f"https://restcountries.com/v3.1/name/{country_name.lower()}")
-    
-    if response.status_code != 200:
-        raise ValueError(f"Error {response.status_code}: {response.reason} ")
+    if response.status_code == 404:
+        raise APIError(message="Unable to locate matching country.", code=404)
+    elif response.status_code == 500:
+        raise APIError(message="Unable to connect to server.")
+    elif response.status_code != 200:
+        raise APIError(message=response.reason, code=response.status_code)
     data = response.json()[0]
-    return f"{data['name']['official']} {data['flag']}\nLanguages: {" ,".join(data['languages'].values())}"
-    
+    country_dict = {}
+    country_dict['name'] = data['name']
+    country_dict['flag'] = data['flag']
+    country_dict['languages'] = data['languages']
+    return country_dict
 
 
 def main():
@@ -41,7 +47,7 @@ def main():
         print(" ")
         try:
             country_data = fetch_data(entry)
-            print_data(country_data)
+            print_data(f"{country_data['name']['official']} {country_data['flag']}\nLanguages: {" ,".join([country_data['languages']])}")
         except APIError as e:
             print(e.message)
         print(" ")
